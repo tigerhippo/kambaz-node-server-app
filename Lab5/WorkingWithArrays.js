c0a33a5c2e87c98cf1320cd1b69b99e69ebdff20
Megan Liu <meganliu@Megans-MacBook.local>
2025-03-29 00:52:53 -0400

    finished everything up until 5.2.4.5

---
+++ Lab5/WorkingWithArrays.js
@@ -0,0 +1,29 @@
+let todos = [ { id: 1, title: "Task 1", completed: false },  { id: 2, title: "Task 2", completed: true },
+              { id: 3, title: "Task 3", completed: false },  { id: 4, title: "Task 4", completed: true }, ];
+
+export default function WorkingWithArrays(app) {
+    app.get("/lab5/todos", (req, res) => {
+        const { completed } = req.query;
+        if (completed !== undefined) {
+            const completedBool = completed === "true";
+            const completedTodos = todos.filter((t) => t.completed === completedBool);
+            res.json(completedTodos);
+            return;
+        }
+        res.json(todos);
+    });
+    app.get("/lab5/todos/create", (req, res) => {
+        const newTodo = {
+          id: new Date().getTime(),
+          title: "New Task",
+          completed: false,
+        };
+        todos.push(newTodo);
+        res.json(todos);
+    });
+    app.get("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        res.json(todo);
+    });
+};
--- Lab5/index.js
+++ Lab5/index.js
@@ -1,6 +1,7 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
@@ -8,4 +9,5 @@ export default function Lab5(app) {
   PathParameters(app);
   QueryParameters(app);
   WorkingWithObjects(app);
+  WorkingWithArrays(app);
 };  
