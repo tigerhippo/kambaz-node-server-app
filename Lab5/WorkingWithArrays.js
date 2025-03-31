8ab161cce52854e0f485bbae48957349697f8717
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-03-30 23:06:03 -0400

    did everything up until 5.2.6.4

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -21,6 +21,11 @@ export default function WorkingWithArrays(app) {
         todos.push(newTodo);
         res.json(todos);
     });
+    app.post("/lab5/todos", (req, res) => {
+        const newTodo = { ...req.body,  id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);
+    });    
     app.get("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
@@ -32,10 +37,26 @@ export default function WorkingWithArrays(app) {
         todos.splice(todoIndex, 1);
         res.json(todos);
     });
+    app.delete("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+    });    
     app.get("/lab5/todos/:id/title/:title", (req, res) => {
         const { id, title } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         todo.title = title;
         res.json(todos);
-    });        
+    });  
+    app.put("/lab5/todos/:id", (req, res) => {
+        const { id } = req.params;
+        todos = todos.map((t) => {
+          if (t.id === parseInt(id)) {
+            return { ...t, ...req.body };
+          }
+          return t;
+        });
+        res.sendStatus(200);
+    });          
 };
