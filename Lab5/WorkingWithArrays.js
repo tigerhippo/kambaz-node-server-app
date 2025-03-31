bf0c713431b194e81d95d8e0a682d61e1914251b
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-03-30 23:53:48 -0400

    did everything up until 5.3

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -40,6 +40,10 @@ export default function WorkingWithArrays(app) {
     app.delete("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+            res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+            return;
+        }      
         todos.splice(todoIndex, 1);
         res.sendStatus(200);
     });    
@@ -51,6 +55,11 @@ export default function WorkingWithArrays(app) {
     });  
     app.put("/lab5/todos/:id", (req, res) => {
         const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+          res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+          return;
+        }    
         todos = todos.map((t) => {
           if (t.id === parseInt(id)) {
             return { ...t, ...req.body };
