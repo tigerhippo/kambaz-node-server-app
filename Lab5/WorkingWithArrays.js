12a4cec641202d4935d4013dd2f154bf508ad9f6
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-03-30 20:51:46 -0400

    did everything up until 5.2.5.6 except for 5.2.4.7

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -26,4 +26,16 @@ export default function WorkingWithArrays(app) {
         const todo = todos.find((t) => t.id === parseInt(id));
         res.json(todo);
     });
+    app.get("/lab5/todos/:id/delete", (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        todos.splice(todoIndex, 1);
+        res.json(todos);
+    });
+    app.get("/lab5/todos/:id/title/:title", (req, res) => {
+        const { id, title } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.title = title;
+        res.json(todos);
+    });        
 };
