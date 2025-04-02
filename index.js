6ccd08287470f9755d76ee2bf1fe52f7f066e91f
Megan Liu <meganliu@Megans-MacBook.local>
2025-04-01 21:04:15 -0400

    did everything up until 5.3.2.3 except for the last part

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kambaz/Users/routes.js";
 const app = express()
+UserRoutes(app);
 app.use(cors());  
 app.use(express.json()); 
 Lab5(app)
