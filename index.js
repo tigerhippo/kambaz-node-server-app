f29e358a1219c60f9b4889b2433b22738a76e7d6
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-04 14:56:31 -0400

    fixed cors statement order

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
 const app = express()
-UserRoutes(app);
+
 app.use(cors());  
 app.use(express.json()); 
+UserRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
