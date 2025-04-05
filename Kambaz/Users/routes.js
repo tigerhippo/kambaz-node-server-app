5154877d6f85819869fecee2cf787d31492e1855
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-04 22:52:05 -0400

    did everything up until 5.3.4

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,5 +1,4 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 export default function UserRoutes(app) {
     const createUser = (req, res) => { };
     const deleteUser = (req, res) => { };
@@ -9,7 +8,8 @@ export default function UserRoutes(app) {
         const userId = req.params.userId;
         const userUpdates = req.body;
         dao.updateUser(userId, userUpdates);
-        currentUser = dao.findUserById(userId);
+        const currentUser = dao.findUserById(userId);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
     const signup = (req, res) => { 
@@ -18,19 +18,30 @@ export default function UserRoutes(app) {
             res.status(400).json({ message: "Username already in use" });
             return;
         }
-        currentUser = dao.createUser(req.body);
+        const currentUser = dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
     const signin = (req, res) => { 
         const { username, password } = req.body;
-        currentUser = dao.findUserByCredentials(username, password);
-        res.json(currentUser);
+        const currentUser = dao.findUserByCredentials(username, password);
+        if (currentUser) {
+            req.session["currentUser"] = currentUser;
+            res.json(currentUser);
+        } else {
+            res.status(401).json({ message: "Unable to login. Try again later." });
+        }
     };
     const profile = async (req, res) => {
+        const currentUser = req.session["currentUser"];
+        if (!currentUser) {
+            res.sendStatus(401);
+            return;
+        }
         res.json(currentUser);
     };
     const signout = (req, res) => {
-        currentUser = null;
+        req.session.destroy();
         res.sendStatus(200);
     };
     
