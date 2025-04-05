51111e22869a9ad224c1e94541f0b1166845d13a
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-04 22:25:21 -0400

    did everything up until 5.3.3

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -5,7 +5,13 @@ export default function UserRoutes(app) {
     const deleteUser = (req, res) => { };
     const findAllUsers = (req, res) => { };
     const findUserById = (req, res) => { };
-    const updateUser = (req, res) => { };
+    const updateUser = (req, res) => {   
+        const userId = req.params.userId;
+        const userUpdates = req.body;
+        dao.updateUser(userId, userUpdates);
+        currentUser = dao.findUserById(userId);
+        res.json(currentUser);
+    };
     const signup = (req, res) => { 
         const user = dao.findUserByUsername(req.body.username);
         if (user) {
@@ -20,8 +26,14 @@ export default function UserRoutes(app) {
         currentUser = dao.findUserByCredentials(username, password);
         res.json(currentUser);
     };
-    const signout = (req, res) => { };
-    const profile = (req, res) => { };
+    const profile = async (req, res) => {
+        res.json(currentUser);
+    };
+    const signout = (req, res) => {
+        currentUser = null;
+        res.sendStatus(200);
+    };
+    
     app.post("/api/users", createUser);
     app.get("/api/users", findAllUsers);
     app.get("/api/users/:userId", findUserById);
