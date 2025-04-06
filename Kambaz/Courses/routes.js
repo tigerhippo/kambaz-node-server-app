552d895dfe81489f44a26eb954cb51186e15d5d7
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-05 21:29:43 -0400

    did everything in backend + get all assignments on frontend in 5.3.6

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,5 +1,6 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
+import * as assignmentsDao from "../Assignments/dao.js";
 export default function CourseRoutes(app) {
     app.get("/api/courses", (req, res) => {
         const courses = dao.findAllCourses();
@@ -9,6 +10,11 @@ export default function CourseRoutes(app) {
         const { courseId } = req.params;
         const modules = modulesDao.findModulesForCourse(courseId);
         res.json(modules);
+    }); 
+    app.get("/api/courses/:courseId/assignments", (req, res) => {
+        const { courseId } = req.params;
+        const assignments = assignmentsDao.findAssignmentsForCourse(courseId);
+        res.json(assignments);
     });    
     app.delete("/api/courses/:courseId", (req, res) => {
         const { courseId } = req.params;
@@ -29,5 +35,14 @@ export default function CourseRoutes(app) {
         };
         const newModule = modulesDao.createModule(module);
         res.send(newModule);
+    });     
+    app.post("/api/courses/:courseId/assignments", (req, res) => {
+        const { courseId } = req.params;
+        const assignment = {
+          ...req.body,
+          course: courseId,
+        };
+        const newAssignment = assignmentsDao.createAssignment(assignment);
+        res.send(newAssignment);
     });        
 }
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
+import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import "dotenv/config";
 import session from "express-session";
 const app = express()
@@ -34,6 +35,7 @@ app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
