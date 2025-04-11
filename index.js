655fe084f2c5e8cdeb9fed906b180d9cbc4b90aa
Megan Liu <meganliu@Megans-MacBook.local>
2025-04-11 18:34:16 -0400

    added db configurations

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import "dotenv/config";
 import session from "express-session";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(
     cors({
