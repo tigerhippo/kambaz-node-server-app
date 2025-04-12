cc72e96d5d3a8de5baf365d8d0f8c557bbbefc16
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-12 19:04:01 -0400

    did everything up until 6.2.6.7

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -3,7 +3,10 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 export default function UserRoutes(app) {
     const createUser = async (req, res) => { };
-    const deleteUser = async (req, res) => { };
+    const deleteUser = async (req, res) => {  
+        const status = await dao.deleteUser(req.params.userId);
+        res.json(status);
+    };
     const findAllUsers = async (req, res) => {
         const { role, name } = req.query;
         if (role) {
@@ -24,11 +27,13 @@ export default function UserRoutes(app) {
         res.json(user);
     };
     const updateUser = async (req, res) => {   
-        const userId = req.params.userId;
+        const { userId } = req.params;
         const userUpdates = req.body;
         await dao.updateUser(userId, userUpdates);
-        const currentUser = await dao.findUserById(userId);
-        req.session["currentUser"] = currentUser;
+        const currentUser = req.session["currentUser"];
+        if (currentUser && currentUser._id === userId) {
+          req.session["currentUser"] = { ...currentUser, ...userUpdates };
+        }     
         res.json(currentUser);
     };
     const signup = async (req, res) => { 
