bb9d1247a9b60fd05d30ee2db87153d1f63cebb9
Megan Liu <meganliu@macbookpro.mynetworksettings.com>
2025-04-12 01:29:19 -0400

    did everything up until 6.2.6.5

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,6 +1,13 @@
 import model from "./model.js";
 export const createUser = (user) => {} // implemented later
 export const findAllUsers = () => model.find();
+export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+};  
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>  model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -5,10 +5,24 @@ export default function UserRoutes(app) {
     const createUser = async (req, res) => { };
     const deleteUser = async (req, res) => { };
     const findAllUsers = async (req, res) => {
+        const { role, name } = req.query;
+        if (role) {
+            const users = await dao.findUsersByRole(role);
+            res.json(users);
+            return;
+        }    
+        if (name) {
+            const users = await dao.findUsersByPartialName(name);
+            res.json(users);
+            return;
+        }
         const users = await dao.findAllUsers();
         res.json(users);
     };
-    const findUserById = async (req, res) => { };
+    const findUserById = async (req, res) => { 
+        const user = await dao.findUserById(req.params.userId);
+        res.json(user);
+    };
     const updateUser = async (req, res) => {   
         const userId = req.params.userId;
         const userUpdates = req.body;
