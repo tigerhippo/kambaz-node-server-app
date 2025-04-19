955b46fd98ac7e9a5d834edc95efd6b4f3376233
Megan Liu <meganliu@Megans-MacBook.local>
2025-04-18 20:46:09 -0400

    did everything up until 6.4.3.5

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,9 +1,14 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
 export default function CourseRoutes(app) {
     app.post("/api/courses", async (req, res) => {
         const course = await dao.createCourse(req.body);
+        const currentUser = req.session["currentUser"];
+        if (currentUser) {
+            await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+        }
         res.json(course);
     });     
     app.get("/api/courses", async (req, res) => {
